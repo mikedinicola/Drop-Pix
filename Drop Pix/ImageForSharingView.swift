d381a8d5d5d50b82b3f7af89c399bc8f90348992
Mike DiNicola <mdinicola@tankdesign.com>
2016-03-05 11:02:40 -0500

    Created ImageForSharingView and accompanying .xib

---
+++ Drop Pix/ImageForSharingView.swift
@@ -0,0 +1,23 @@
+//
+//  ImageForSharingView.swift
+//  Drop Pix
+//
+//  Created by Mike DiNicola on 3/5/16.
+//  Copyright © 2016 Mike DiNicola. All rights reserved.
+//
+
+import UIKit
+
+class ImageForSharingView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
